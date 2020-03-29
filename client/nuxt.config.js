96271a920006037ab4f873ac39ffa3b708b8fa73
Cyril de Wit <cyrilwit@gmail.com>
2020-03-29 18:57:29 +0200

    chore: setup

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,6 +1,7 @@
 
 export default {
   mode: 'spa',
+
   /*
   ** Headers of the page
   */
@@ -12,49 +13,108 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#fff' },
+
   /*
   ** Global CSS
   */
   css: [
   ],
+
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/font-awesome',
+    '~/plugins/vee-validate',
   ],
+
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/moment',
     '@nuxtjs/tailwindcss',
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
+    'nuxt-i18n',
     '@nuxtjs/axios',
-    // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
+    '@nuxtjs/auth',
+    'nuxt-validate',
   ],
+
+  moment: {
+    defaultLocale: 'nl',
+    locales: ['nl'],
+  },
+
+  i18n: {
+    locales: [
+      {
+        name: 'Dutch',
+        code: 'nl',
+        iso: 'nl-NL',
+        file: 'nl-NL.js',
+      },
+      {
+        name: 'English',
+        code: 'en',
+        iso: 'en-GB',
+        file: 'en-GB.js',
+      },
+    ],
+    defaultLocale: 'nl',
+    lazy: true,
+    langDir: 'locales/',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+    },
+    vueI18n: {
+      fallbackLocale: 'nl',
+    }
+  },
+
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
   },
+
+  nuxtValidate: {
+    lang: 'nl',
+    nuxti18n: {
+      locale: {
+        'nl-NL': 'nl_NL',
+      }
+    }
+  },
+
   /*
   ** Build configuration
   */
   build: {
+    postcss: {
+      plugins: {
+        'postcss-import': {},
+        'postcss-url': {},
+        'postcss-nested': {},
+        'postcss-simple-vars': {},
+      },
+    },
+
     /*
     ** You can extend webpack config here
     */
