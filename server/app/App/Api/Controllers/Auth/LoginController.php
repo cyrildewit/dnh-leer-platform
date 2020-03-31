bec0f9b60598736cb83d6dd8bb5041150dc06046
Cyril de Wit <cyrilwit@gmail.com>
2020-03-31 15:56:18 +0200

    feat: add api auth routes

---
+++ server/app/App/Api/Controllers/Auth/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Api\Controllers\Auth;
+
+use Support\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+// use App\Exceptions\VerifyEmailException;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+final class LoginController extends Controller
+{
+    use ValidatesRequests, AuthenticatesUsers;
+
+    public function __construct()
+    {
+        $this->middleware('guest:api')->except('logout');
+    }
+
+    protected function attemptLogin(Request $request)
+    {
+        $token = $this->guard()->attempt($this->credentials($request));
+
+        if (! $token) {
+            return false;
+        }
+
+        $user = $this->guard()->user();
+
+        $this->guard()->setToken($token);
+
+        return true;
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+
+        $token = (string) $this->guard()->getToken();
+        $expiration = $this->guard()->getPayload()->get('exp');
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $expiration - time(),
+        ]);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $user = $this->guard()->user();
+
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('api');
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Api\Controllers\Auth\LoginController;
+use App\Api\Controllers\Auth\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [LoginController::class, 'login']);
+        // Route::post('register', 'RegisterController@register');
+        Route::get('user', [UserController::class, 'authenticated']);
+    });
 });
