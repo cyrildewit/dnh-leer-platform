f267c2a29d2f825b084f596144cb7855360a46e2
Cyril de Wit <cyrilwit@gmail.com>
2020-04-01 14:44:32 +0200

    feat: update footer

--- client/components/Layouts/BrandDefault/SiteFooter.vue
+++ client/components/Layouts/BrandDefault/SiteFooter.vue
@@ -1,6 +1,10 @@
 <template>
   <footer>
-    <p>Copyright &copy; 2020 - Leer Platform</p>
+     <div class="mt-4 border-t border-gray-400 flex flex-col items-center">
+      <div class="container mx-auto px-6">
+        <p class="py-6 text-sm text-center">&copy; <span>{{ (new Date()).getFullYear() }}</span> by Leer Platform</p>
+      </div>
+    </div>
   </footer>
 </template>
 
